9c45fc2373292b9031b3c9c9d77d053d36348f2f
mommomoaz <fnskwhdk@gmail.com>
2023-07-27 23:55:36 +0900

    problem solved

---
+++ Basic/p9498.js
@@ -0,0 +1,17 @@
+const input = 89;
+let result = null;
+if(89<input && input<=100){
+    result="A";
+}else if (79<input&&input<=89){
+    result="B";
+}else if (69<input&&input<=79){
+    result="C";
+}else if(59<input&&input<=69){
+    result="D";
+}else{
+    result="F";
+}
+
+console.log(result);
+
+
