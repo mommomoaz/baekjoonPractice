f6d06c3b8bf7432490fe1954e80d50293b26acf5
fnskwhdk9@gmail.com <fnskwhdk9@gmail.com>
2023-07-31 22:07:58 +0900

    resolved

--- Basic/p10926.js
+++ Basic/p10926.js
@@ -5,7 +5,7 @@ if (!/^[a-z]+$/.test(input)) {
 } else if (input.length > 50) {
     console.log("아이디는 50자를 넘지 않아야 합니다.");
 }
-if (input === "joonas") {
+if (input === "baekjoon") {
     console.log(input + "??!");
 } else {
     console.log("ok");
