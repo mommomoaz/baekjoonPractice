0bdcd71383a9edca14a68a4df8102736f091b9e2
mommomoaz <fnskwhdk@gmail.com>
2023-07-31 21:59:48 +0900

    solved

---
+++ Basic/p10926.js
@@ -0,0 +1,14 @@
+let input = prompt("입력");
+
+if (!/^[a-z]+$/.test(input)) {
+    console.log("아이디는 알파벳 소문자로만 이루어져야 합니다.");
+} else if (input.length > 50) {
+    console.log("아이디는 50자를 넘지 않아야 합니다.");
+}
+if (input === "joonas") {
+    console.log(input + "??!");
+} else {
+    console.log("ok");
+}
+
+
