5c5453517974bbfd48cc6b71b6522403f68b2fd6
fnskwhdk9@gmail.com <fnskwhdk9@gmail.com>
2023-08-13 19:28:22 +0900

    problem solved

--- Basic/p10952.js
+++ Basic/p10952.js
@@ -4,10 +4,9 @@ let B = Number(prompt("Please enter a numberB:"));
 while(A!=0&&B!=0){
     if(A>0 && A<10&&B>0&&B<10){
         console.log(A+B)
+        A = Number(prompt("Please enter a numberA:"));
+        B = Number(prompt("Please enter a numberB:"));
     };
-    A = Number(prompt("Please enter a numberA:"));
-    B = Number(prompt("Please enter a numberB:"));
-
 }
 
 
